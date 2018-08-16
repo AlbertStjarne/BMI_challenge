cbec452c8f08b3cd97e7192874b9af267e3e1e58
Albert Stjärne <albert.stjarne@gmail.com>
2018-08-16 20:06:25 +0200

    Metric code and spec finalized

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -1,6 +1,9 @@
+// spec.helper.js
+
 const fs = require('fs');       //to read from file system
 const chai = require('chai');   //
 global.expect = chai.expect;    //global variable called expect, 
 
+let calc = fs.readFileSync('./src/js/calculator.js');
 let person = fs.readFileSync('./src/js/person.js');
-eval( person + `\nexports.Person = Person;`)        //calling method eval. Evaluating the content of person
+eval( calc + person + `\nexports.Person = Person;\nexports.BMICalculator = BMICalculator;`)        //calling method eval. Evaluating the content of person
